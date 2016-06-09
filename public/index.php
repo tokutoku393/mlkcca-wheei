4801b757d16a65e1e80cf481bde21a0a51ce1cbd
yuka <la_boo_u@live.jp>
2016-06-09 21:14:04 +0900

    fix typo

--- public/index.php
+++ public/index.php
@@ -7,16 +7,16 @@ require __DIR__ . '/../vendor/autoload.php';
 $env = new Dotenv\Dotenv(__DIR__ . '/../');
 $env->load();
 
-$app = new \Slim\slim(array(
+$app = new \Slim\Slim(array(
   'debug' => true,
   'view' => new \Slim\Views\Smarty(),
   'log.writer' => new \Slim\Logger\DateTimeFileWriter(array('path' => getenv('BASE_DIR').'/storage/logs')),
 ));
 
-$app->response->headers->set('X-FRAME-OPTIONS', 'DENY');
+//$app->response->headers->set('X-FRAME-OPTIONS', 'DENY');
 $view = $app->view();
 $log = $app->log;
-$view->setTemplatesDirectory(getenv('BASE_DIR' . '/app/templates'));
+$view->setTemplatesDirectory(getenv('BASE_DIR') . '/app/templates');
 $view->parserCompileDirectory = getenv('BASE_DIR') . '/storage/compiled';
 $view->parserCacheDirectory = getenv('BASE_DIR') . '/storage/cache';
 $view->parserExtensions = array(getenv('BASE_DIR') . '/vendor/slim/views/SmartyPl
